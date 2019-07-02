fbcfe2f8595702bc83a71ae4f4349e93e74e5fd9
Aldiyar Batyrbekov <aldibatyrbekov@gmail.com>
2019-07-01 19:22:35 -0700

    Question form work in progress

---
+++ src/routes/StrengthQuestion.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function StrengthQuestion() {
+  return(
+    <div className='question'>
+      <h2>How strong would you like your hookah to be?</h2>
+      <li>
+        <input
+        type='radio'
+        className='radioButton'
+        name='radioGroup'/>
+        <label>
+
+        </label>
+      </li>
+    </div>
+  )
+}
